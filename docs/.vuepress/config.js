628d519939a775f63c856059aea6c7f0ede63c65
yinjialu <yinjialu1213@gmail.com>
2019-04-04 14:49:59 +0800

    feat: 新增webstorm使用示例截图

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    title: '前端日志',
+    description: '记录，总结，分享，javascript，node',
+    base: '/blog-book/',
+    head: [
+        ['link', { rel: 'icon', href: './dragon.png' }]
+    ]
+};
