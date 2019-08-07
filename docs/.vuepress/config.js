d520f0221d91a3be7e3664bf83dbdbcfe122c62d
yinjialu <yinjialu1213@gmail.com>
2019-08-07 10:21:41 +0800

    feat: 更新导航

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     base: '/blog-book/',
-    title: '前端日志',
+    title: '前端笔记',
     description: '记录，总结，分享，javascript，node',
     head: [
         ['link', { rel: 'icon', href: '/dragon.png' }]
@@ -11,23 +11,75 @@ module.exports = {
         docsDir: 'docs',
         editLinks: true,
         editLinkText: '在 GitHub 上编辑此页',
-        lastUpdated: '上次更新',
+        // lastUpdated: '上次更新',
         nav: [],
         sidebar: [
             {
-                title: '准备工作',
+                title: '持续优化',
+                collapsable: false,
+                children: [
+                    'optimize/cache',
+                    'optimize/optimize',
+                    'optimize/prerender',
+                ]
+            }, {
+                title: 'node',
+                collapsable: false,
+                children: [
+                    'node/npm',
+                    'node/stream',
+                    'node/cookie',
+                    'node/cors',
+                    'node/download',
+                    'node/require',
+                    'node/path',
+                    'node/contos',
+                ],
+            }, {
+                title: 'Vue',
+                collapsable: false,
+                children: [
+                    'vue/v-model',
+                    'vue/vueRouterHistory',
+                ]
+            }, {
+                title: '异步',
+                collapsable: false,
+                children: [
+                    'async/callback',
+                    'async/promise',
+                    'async/async',
+                ]
+            }, {
+                title: '工具收藏',
+                collapsable: false,
+                children: [
+                    'tool/driver',
+                    'tool/validator',
+                ],
+            }, {
+                title: '好记性不如烂笔头',
+                collapsable: false,
+                children: [
+                    'useful/charles',
+                    'useful/codetype',
+                    'useful/favicon',
+                    'useful/git',
+                    'useful/github',
+                    'useful/webstorm',
+                ]
+            }, {
+                title: '未分类',
                 collapsable: false,
                 children: [
                     ['note/', 'Introduction'],
-                    'note/codetype',
-                    'note/favicon',
                     'note/this',
                     'note/闭包和高阶函数',
                     'note/singleton',
                     'note/socket.io',
-                    'note/cache'
+                    'note/es6'
                 ]
-            }
+            },
         ]
     }
 };
