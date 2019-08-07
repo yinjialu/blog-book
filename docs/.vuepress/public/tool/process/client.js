22d160a5e8c54445336801a6da149765e37314c3
yinjialu <yinjialu1213@gmail.com>
2019-08-07 09:50:33 +0800

    feat: node 子线程的示例函数

---
+++ docs/.vuepress/public/tool/process/client.js
@@ -0,0 +1,46 @@
+'use strict';
+const path = require('path');
+const net = require('net');
+const Parser = require('./parser');
+const EventEmitter = require('events');
+const os = require('os');
+const tmpDir = os.tmpdir();
+let sockPath = path.join(tmpDir, 'midway.sock');
+
+if (process.platform === 'win32') {
+    sockPath = sockPath.replace(/^\//, '');
+    sockPath = sockPath.replace(/\//g, '-');
+    sockPath = '\\\\.\\pipe\\' + sockPath;
+}
+
+class Client extends EventEmitter{
+    constructor(options) {
+        options = options || {};
+        super();
+        if (options.socket) {
+            this.socket = options.socket;
+        } else {
+            this.socket = net.connect(sockPath);
+        }
+        this.bind();
+    }
+
+    bind() {
+        const parser = new Parser();
+        const socket = this.socket;
+        socket.on('data', (buf) => {
+            parser.feed(buf);
+        });
+
+        parser.on('message', (message) => {
+            this.emit('message', message);
+        });
+        this.parser = parser;
+    }
+
+    send(message) {
+        this.socket.write(this.parser.encode(message));
+    }
+}
+
+module.exports = Client;
